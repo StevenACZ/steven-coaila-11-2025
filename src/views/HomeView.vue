92d9d2c05a6cba576edb1dd0bb4bacdb1cbc6fbd
steven <scoaila@proton.me>
2025-12-11 15:37:04 -0500

    initial setup

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="home">
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.home {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+
+  h1 {
+    font-size: 2rem;
+  }
+}
+</style>
